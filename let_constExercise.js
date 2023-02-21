0a9ef479cf382cb8321d1c36628f93a5b263e8d8
marxmoyer <marxmoyer@gmail.com>
2023-02-21 15:36:00 -0700

    first commit

---
+++ let_constExercise.js
@@ -0,0 +1,19 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+PI = 42; // error const cant be  reassign
+
+// What is the difference between var and let?
+//you can reassign var to a differnt value with the same variable name 
+// you cant with let, var also is a hoisted variable 
+
+// What is the difference between var and const?
+//  you cant reassign to const like you can to var,
+//  var also is a hoisted variable, and const is block scope
+// What is the difference between let and const?
+//you cant reassign to const like you can to let
+
+// What is hoisting?
+// hoisting it the abilty to access a veriable before its declared 
+// because its as if it was defined at the top of the page
